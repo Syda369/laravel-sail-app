0aea2c2fc35be988bbbb49a00a03da802a8dabb0
Syda369 <106336670+Syda369@users.noreply.github.com>
2022-05-30 01:06:22 +0300

    header update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
                         </g>
                     </svg>
                 </div>
-                <h1>مرحبا أنا إسمي سيده</h1>
+                <h1  style="text-align:center">مرحبا أنا إسمي سيده</h1>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
